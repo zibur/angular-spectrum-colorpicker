ec0a5ce44fdefd4df360cf34e84019f7d2593298
Hannes Diercks <hi@xiphe.net>
2014-03-05 17:06:57 +0100

    Release v1.0.7

--- dist/angular-spectrum-colorpicker.js
+++ dist/angular-spectrum-colorpicker.js
@@ -1,5 +1,5 @@
 /*!
- * angular-spectrum-colorpicker v1.0.6
+ * angular-spectrum-colorpicker v1.0.7
  * https://github.com/Jimdo/angular-spectrum-colorpicker
  *
  * Angular directive for a colorpicker, that bases on http://bgrins.github.io/spectrum/
