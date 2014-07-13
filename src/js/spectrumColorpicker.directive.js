20ddeb43cfdd57d512f6072323dd59db5119f2dd
Joscha Feth <joscha@feth.com>
2014-07-13 20:35:18 +0200

    make sure undefined is passed in

--- src/js/spectrumColorpicker.directive.js
+++ src/js/spectrumColorpicker.directive.js
@@ -1,64 +1,67 @@
-angularSpectrumColorpicker.directive('spectrumColorpicker', function() {
-  return {
-    restrict: 'E',
-    require: 'ngModel',
-    scope: false,
-    replace: true,
-    template: '<span><input class="input-small" /></span>',
-    link: function($scope, $element, attrs, $ngModel) {
+(function(undefined) {
+  'use strict';
+  angularSpectrumColorpicker.directive('spectrumColorpicker', function() {
+    return {
+      restrict: 'E',
+      require: 'ngModel',
+      scope: false,
+      replace: true,
+      template: '<span><input class="input-small" /></span>',
+      link: function($scope, $element, attrs, $ngModel) {
 
-      var $input = $element.find('input');
-      var fallbackValue = $scope.$eval(attrs.fallbackValue);
-      var format = attrs.format || undefined;
+        var $input = $element.find('input');
+        var fallbackValue = $scope.$eval(attrs.fallbackValue);
+        var format = attrs.format || undefined;
 
-      function setViewValue(color) {
-        var value = fallbackValue;
+        function setViewValue(color) {
+          var value = fallbackValue;
 
-        if (color) {
-          value = color.toString(format);
-        } else if (angular.isUndefined(fallbackValue)) {
-          value = color;
+          if (color) {
+            value = color.toString(format);
+          } else if (angular.isUndefined(fallbackValue)) {
+            value = color;
+          }
+
+          $ngModel.$setViewValue(value);
         }
 
-        $ngModel.$setViewValue(value);
-      }
+        var onChange = function(color) {
+          $scope.$apply(function() {
+            setViewValue(color);
+          });
+        };
+        var onToggle = function() {
+          $input.spectrum('toggle');
+          return false;
+        };
+        var options = angular.extend({
+          color: $ngModel.$viewValue,
+          change: onChange,
+          move: onChange,
+          hide: onChange
+        }, $scope.$eval(attrs.options));
 
-      var onChange = function(color) {
-        $scope.$apply(function() {
-          setViewValue(color);
+
+        if(attrs.triggerId) {
+          angular.element(document.body).on('click', '#' + attrs.triggerId, onToggle);
+        }
+
+
+        $ngModel.$render = function() {
+          $input.spectrum('set', $ngModel.$viewValue || '');
+        };
+
+        if (options.color) {
+          $input.spectrum('set', options.color || '');
+          setViewValue(options.color);
+        }
+
+        $input.spectrum(options);
+
+        $scope.$on('$destroy', function() {
+          $input.spectrum('destroy');
         });
-      };
-      var onToggle = function() {
-        $input.spectrum('toggle');
-        return false;
-      };
-      var options = angular.extend({
-        color: $ngModel.$viewValue,
-        change: onChange,
-        move: onChange,
-        hide: onChange
-      }, $scope.$eval(attrs.options));
-
-
-      if(attrs.triggerId) {
-        angular.element(document.body).on('click', '#' + attrs.triggerId, onToggle);
       }
-
-
-      $ngModel.$render = function() {
-        $input.spectrum('set', $ngModel.$viewValue || '');
-      };
-
-      if (options.color) {
-        $input.spectrum('set', options.color || '');
-        setViewValue(options.color);
-      }
-
-      $input.spectrum(options);
-
-      $scope.$on('$destroy', function() {
-        $input.spectrum('destroy');
-      });
-    }
-  };
-});
+    };
+  });
+})();
