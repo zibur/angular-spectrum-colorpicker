5767f6dd5bc0be54f15ea15928b516d96406d432
Joscha Feth <joscha@feth.com>
2014-07-28 19:43:16 +0200

    Release v1.0.16

--- dist/angular-spectrum-colorpicker.js
+++ dist/angular-spectrum-colorpicker.js
@@ -1,5 +1,5 @@
 /*!
- * angular-spectrum-colorpicker v1.0.15
+ * angular-spectrum-colorpicker v1.0.16
  * https://github.com/Jimdo/angular-spectrum-colorpicker
  *
  * Angular directive for a colorpicker, that bases on http://bgrins.github.io/spectrum/
