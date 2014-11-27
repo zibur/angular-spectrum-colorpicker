c914028ea8c932f1cea2a25e866e5938479310f3
Joscha Feth <joscha@feth.com>
2014-11-27 15:04:14 +0100

    Release v1.3.0

--- dist/angular-spectrum-colorpicker.js
+++ dist/angular-spectrum-colorpicker.js
@@ -1,5 +1,5 @@
 /*!
- * angular-spectrum-colorpicker v1.2.1
+ * angular-spectrum-colorpicker v1.3.0
  * https://github.com/Jimdo/angular-spectrum-colorpicker
  *
  * Angular directive for a colorpicker, that bases on http://bgrins.github.io/spectrum/
@@ -21,25 +21,50 @@
       return {
         restrict: 'E',
         require: 'ngModel',
-        scope: false,
+        scope: {
+          fallbackValue: '=',
+          format: '=?',
+          options: '=?',
+          triggerId: '@?',
+  
+          onChange: '&?',
+          onShow: '&?',
+          onHide: '&?',
+          onMove: '&?',
+  
+          onBeforeShow: '&?',
+        },
         replace: true,
         templateUrl: 'directive.html',
         link: function($scope, $element, attrs, $ngModel) {
   
           var $input = $element.find('input');
-          var fallbackValue = $scope.$eval(attrs.fallbackValue);
-          var format = $scope.$eval(attrs.format) || undefined;
+  
+          function formatColor(tiny) {
+            var formatted = tiny;
+            if (formatted) {
+              formatted = tiny.toString($scope.format);
+            }
+            return formatted;
+          }
+  
+          function callOnChange(color) {
+            if (angular.isFunction($scope.onChange)) {
+              $scope.onChange({color: color});
+            }
+          }
   
           function setViewValue(color) {
-            var value = fallbackValue;
+            var value = $scope.fallbackValue;
   
             if (color) {
-              value = color.toString(format);
-            } else if (angular.isUndefined(fallbackValue)) {
+              value = formatColor(color);
+            } else if (angular.isUndefined($scope.fallbackValue)) {
               value = color;
             }
   
             $ngModel.$setViewValue(value);
+            callOnChange(value);
           }
   
           var onChange = function(color) {
@@ -51,21 +76,49 @@
             $input.spectrum('toggle');
             return false;
           };
-          var options = angular.extend({
-            color: $ngModel.$viewValue,
-            change: onChange,
-            move: onChange,
-            hide: onChange
-          }, $scope.$eval(attrs.options));
   
   
-          if(attrs.triggerId) {
-            angular.element(document.body).on('click', '#' + attrs.triggerId, onToggle);
+          var baseOpts = {
+            color: $ngModel.$viewValue
+          };
+  
+          var localOpts = {};
+  
+          angular.forEach({
+            'change': 'onChange',
+            'move': 'onMove',
+            'hide': 'onHide',
+            'show': 'onShow'
+          }, function(eventKey, spectrumOptionName) {
+            localOpts[spectrumOptionName] = function(color) {
+              onChange(color);
+              // we don't do this for change, because we expose the current
+              // value actively through the model
+              if (eventKey !== 'change' && angular.isFunction($scope[eventKey])) {
+                return $scope[eventKey]({color: formatColor(color)});
+              }
+            };
+          });
+  
+          if (angular.isFunction($scope.onBeforeShow)) {
+            localOpts.beforeShow = function(color) {
+              return $scope.onBeforeShow({color: formatColor(color)});
+            };
           }
   
+          var options = angular.extend({}, baseOpts, $scope.options, localOpts);
+  
+          function getTriggerElement() {
+            return angular.element(document.body).find('#' + $scope.triggerId);
+          }
+  
+          if ($scope.triggerId) {
+            getTriggerElement().on('click', onToggle);
+          }
   
           $ngModel.$render = function() {
             $input.spectrum('set', $ngModel.$viewValue || '');
+            callOnChange($ngModel.$viewValue);
           };
   
           if (options.color) {
@@ -76,6 +129,9 @@
           $input.spectrum(options);
   
           $scope.$on('$destroy', function() {
+            if ($scope.triggerId) {
+              getTriggerElement().off('click', onToggle);
+            }
             $input.spectrum('destroy');
           });
         }
